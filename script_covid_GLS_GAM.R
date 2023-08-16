baf2bcb874c74ddba87e5ff88828bc79c63982f4
Issengard83 <tamararicardo83@gmail.com>
2023-08-16 08:50:11 -0300

    Uploaded 16/08/23

---
+++ script_covid_GLS_GAM.R
@@ -0,0 +1,239 @@
+#### LOAD PACKAGES ####
+pacman::p_load(tidyverse,readxl,gtsummary,corrplot,flextable,nlme,mgcv, 
+               DHARMa,performance,emmeans,itsadug,gratia)
+
+#### LOAD DATASET ####
+DF = readxl::read_excel("COVID_PAMI_mod.xlsx") %>%
+  mutate(
+    # Tiempo a factor
+    Tiempo_cat = factor(Tiempo),
+    # Edad a factor
+    Edad_cat = gtools::quantcut(Edad, q = 2),
+    # UA logaritmica
+    UA_log = log(UA_mod),
+    # Tratamiento (grupo:vacuna)
+    Tratamiento = fct_cross(Grupo,Vacuna) %>% 
+      fct_relevel("Naïve:Sinopharm","Naïve:Sputnik V", after = 1),
+    # Variables categóricas a factor
+    across(where(is.character), as.factor)) %>% 
+  # Elimina registros con datos faltantes para comorbilidades
+  filter(!is.na(Diabetes)) %>% droplevels()
+
+### Descripción grupos----
+DF %>% filter(Tiempo==0) %>% select(Grupo, Vacuna) %>% 
+  tbl_cross() %>% bold_labels()
+
+### Descripción edades----
+compareGroups::descrTable(Sexo ~ Edad_cat, data = DF, method = 4, show.all = T)
+
+### Spaghetti plots----
+ggpubr::ggarrange(
+  # variable respuesta: UA
+  ggplot(DF, aes(x = factor(Tiempo), y = UA_mod, group = ID, color = Tratamiento)) +
+    geom_line() +
+    labs(y = "UA", x = "Tiempo (días)") +
+    scale_color_brewer(palette = "Set2") +
+    theme_light()
+  ,
+  # Variable respuesta: log(UA)
+  ggplot(DF, aes(x = factor(Tiempo), y = UA_log, group = ID, color = Tratamiento)) +
+    geom_line() +
+    labs(y = "log(UA)", x = "Tiempo (días)") +
+    scale_color_brewer(palette = "Set2") +
+    theme_light(), 
+  common.legend = T, legend = "bottom", labels = "AUTO",font.label = list(size=12))
+
+### Interacción entre grupo y vacuna----
+fit = lme(UA_log ~ Grupo*Vacuna, random = ~1|ID, data = DF)
+anova(fit)
+
+### FIG. 1: Gráfico de perfiles----
+ggplot(data = DF, aes(x = Tiempo, y = UA_log, color = Tratamiento, group = Tratamiento)) +
+  geom_smooth(method = "loess", se = F) + 
+  scale_color_brewer(palette = "Set2") +
+  labs(y = "log(UA)") +
+  scale_x_continuous(breaks = c(0,21,42,120,180), minor_breaks = F) +
+  theme_light() + theme(legend.position = "bottom", legend.title = element_blank())
+
+ggsave("FIGS/Fig1.svg", width = 16, height = 10, units = "cm", dpi = 300)
+
+### Tabla S1: Asociación con VE----
+DF %>% select(Sexo, Edad_cat, COVID_estudio, Brote_estudio, Diabetes, 
+              HTA, EPOC, Obesidad_severa, Enf_renal_cronica, Insuf_cardiaca,
+              Inmunodeficiencia, UA_log, ID) %>% 
+  tbl_uvregression(y = UA_log, method = glmmTMB::glmmTMB,
+                   formula = "{y} ~ {x} + (1|ID)",
+                   pvalue_fun = function(x) style_pvalue(x, digits = 2),
+                   show_single_row = -UA_log) %>% 
+  bold_p() %>% 
+  as_flex_table() %>% 
+  save_as_docx(path = "FIGS/tabS1.docx")
+
+### Asociación entre VE----
+require(compareGroups)
+
+# Sexo
+descrTable(Sexo ~ Edad_cat + # asoc. significativa
+             COVID_estudio +
+             Brote_estudio +
+             Enf_renal_cronica + # asoc. significativa
+             Insuf_cardiaca + 
+             Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+# Grupo etario
+descrTable(Edad_cat ~ COVID_estudio +
+             Brote_estudio +
+             Enf_renal_cronica +
+             Insuf_cardiaca + 
+             Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+# COVID durante el estudio
+descrTable(COVID_estudio ~ Brote_estudio + # asoc. significativa
+             Enf_renal_cronica + 
+             Insuf_cardiaca + # asoc. significativa
+             Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+# Brote durante el estudio
+descrTable(Brote_estudio ~ Enf_renal_cronica + # asoc. significativa
+             Insuf_cardiaca + 
+             Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+# Enfermedad renal crónica
+descrTable(Enf_renal_cronica ~ Insuf_cardiaca + # asoc. significativa
+             Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+# Insuficiencia cardíaca
+descrTable(Insuf_cardiaca ~ Inmunodeficiencia, 
+           data = DF %>% filter(Tiempo==0))
+
+### FIG. S2: Autocorrelación temporal----
+require(corrplot)
+# svg(filename = "FIGS/FigS2.svg", width = 4, height = 4)
+DF %>% select(ID, Tiempo, UA_log) %>% 
+  pivot_wider(values_from = "UA_log", names_from = "Tiempo") %>% select(-ID) %>% 
+  cor(., use = "na.or.complete") %>% 
+  corrplot(type = "upper", addCoef.col = "black",outline = F,
+           diag = F, 
+           mar = c(1,1,1,1), tl.col = "black",
+           col = COL2("PuOr"))
+# dev.off()
+
+#### MODELOS MARGINALES (GLS) ####
+### Autorregresiva continua de orden 1 c/varianza homogénea
+gls1 = gls(UA_log ~ Tratamiento*Tiempo_cat +
+             Edad_cat + Brote_estudio + Insuf_cardiaca + Inmunodeficiencia,
+           correlation = corCAR1(form = ~1|ID), 
+           data = DF)
+
+### Autorregresiva continua de orden 1 c/varianza heterogénea
+gls2 = gls(UA_log ~ Tratamiento*Tiempo_cat +
+             Edad_cat + Brote_estudio + Insuf_cardiaca + Inmunodeficiencia,
+           correlation = corCAR1(form = ~1|ID), 
+           weights = varIdent(form= ~ 1|Tiempo_cat),
+           data = DF)
+
+### Compara modelos marginales----
+compare_performance(gls1, gls2, metrics = "all", rank = F)
+
+### Selección variables explicativas----
+anova(gls2)
+gls2a = update(gls2, ~.-Insuf_cardiaca)
+anova(gls2a)
+gls2b = update(gls2a, ~.-Edad_cat)
+anova(gls2b)
+gls2c = update(gls2b, ~.-Inmunodeficiencia)
+anova(gls2c)
+
+### Tabla 1: Selección de variables
+MuMIn::model.sel(gls2, gls2a, gls2b, gls2c, rank = "AIC")
+
+### Limpia environment
+rm(list = setdiff(ls(),c("DF","gls2c")))
+
+### Análisis residuales----
+check_model(gls2c)
+
+### Test de comparaciones múltiples----
+em = emmeans(gls2c, ~ Tratamiento * Tiempo_cat + Brote_estudio)
+
+# Resumen
+pairs(em, infer = T, simple = "Tratamiento") %>% 
+  as.data.frame() %>% 
+  mutate_if(is.numeric, round, 3) %>% 
+  flextable() %>% save_as_html(path = "emmeans_tab.html")
+
+## FIG. S3: Presentación resultados test comparaciones múltiples----
+g1 = ggeffects::ggemmeans(model = gls2c,
+                          terms = c("Tiempo_cat","Tratamiento","Brote_estudio"))
+
+ggplot(g1, aes(x = x %>% as.character %>% as.numeric, y = predicted, color = group)) +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+  geom_line() +
+  scale_color_brewer(palette = "Set2") +
+  facet_wrap(.~ facet, ncol = 1,
+             labeller = labeller(facet = c(No = "Brote estudio: No", Si = "Brote estudio: Si"))) +
+  scale_x_continuous(breaks = c(0,21,42,120,180)) +
+  labs(x = "Tiempo (días)", y = "log(UA)") +
+  theme_minimal() + theme(legend.position = "bottom", legend.title = element_blank(),
+                          axis.title = element_text(face = "bold", size = 10),
+                          strip.text = element_text(face = "bold", size = 10))
+
+# ggsave("FIGS/FigS3.svg", width = 16, height = 20, units = "cm", dpi = 300)
+
+#### MODELA RESPUESTA NO LINEAL (GAMM) ####
+### GAMM c/corCAR1----
+gamm = gamm(UA_log ~ Brote_estudio + Tratamiento +
+              s(Tiempo, by = Tratamiento, k = 5),
+            random = list(ID = ~ 1),
+            correlation = corCAR1(form = ~1|ID), 
+            weights = varIdent(form= ~ 1|Tiempo_cat),
+            data = DF, method = "REML")
+
+summary(gamm$gam)
+summary(gamm$lme)
+
+### Análisis residuales----
+acf(gamm$lme %>% residuals())
+pacf(gamm$lme %>% residuals())
+
+appraise(gamm$gam)
+
+### Grados de libertad efectivos----
+edf(gamm$gam)
+
+### FIG. 2: Grafica curvas suavizadas----
+svg(filename = "FIGS/Fig2.svg", width = 6.3, height = 7.9)
+par(mfrow = c(2,1), cex = .75)
+# Brote: Si
+plot_smooth(gamm$gam, view = "Tiempo", plot_all = "Tratamiento", 
+            cond = list(Brote_estudio="Si"), v0 = c(0,21,42,120,180),
+            rug = F, rm.ranef = F,legend_plot_all = NULL, ylim = c(2,11),
+            ylab = "log(UA)", main = "Brote COVID-19: Si",
+            col = c("#66C2A5","#FC8D62","#8DA0CB","#E78AC3","#A6D854","#FFD92F"))
+
+# Brote: No
+plot_smooth(gamm$gam, view = "Tiempo", plot_all = "Tratamiento", 
+            cond = list(Brote_estudio="No"), v0 = c(0,21,42,120,180),
+            rug = F, rm.ranef = F,legend_plot_all = F, ylim = c(2,11),
+            ylab = "log(UA)",main = "Brote COVID-19: No",
+            col = c("#66C2A5","#FC8D62","#8DA0CB","#E78AC3","#A6D854","#FFD92F"))
+
+dev.off()
+
+### FIG. 3: Gráfico de los términos paramétricos----
+# svg(filename = "FIGS/Fig3.svg", width = 6.3, height = 7.9)
+par(mfrow = c(2,1), cex = .75)
+plot_parametric(gamm$gam, pred = list(Tratamiento = levels(DF$Tratamiento)),
+                cond = list(Brote_estudio="Si"), xlim = c(4, 12),
+                main = "Brote COVID-19: Si", xlab = "log(UA)")
+plot_parametric(gamm$gam, pred = list(Tratamiento = levels(DF$Tratamiento)),
+                cond = list(Brote_estudio="No"),  xlim = c(4, 12),
+                main = "Brote COVID-19: No", xlab = "log(UA)")
+
+# dev.off()
+
